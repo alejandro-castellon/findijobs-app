dd9fea7ee3345a32cb759d66cdf25da77e54cea6
Alejandro Castellon <alejandrocastellonfer@gmail.com>
2024-05-15 20:59:01 -0400

    add theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { MenuIcon } from "lucide-react";
+import { ModeToggle } from "../ui/theme-toggle";
 
 type Props = {};
 
 const Navbar = async (props: Props) => {
   return (
-    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+    <header className="fixed right-0 left-0 top-0 py-4 px-4 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-blue-900 justify-between">
       <aside className="flex items-center gap-4">
         <Image
           src="/findijobsLogo.png"
           width={208}
           height={166}
           alt="FindiJobs logo"
-          className="shadow-sm"
         />
       </aside>
       <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
@@ -44,13 +44,14 @@ const Navbar = async (props: Props) => {
           href="/dashboard"
           className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
         >
-          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#FFFFFF_0%,#1e81cd_50%,#FFFFFF_100%)]" />
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
             {true ? "Dashboard" : "Get Started"}
           </span>
         </Link>
         {/*user ? <UserButton afterSignOutUrl="/" /> : null */}
         <MenuIcon />
+        <ModeToggle />
       </aside>
     </header>
   );
